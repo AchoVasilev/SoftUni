b18c02d50f8894eddc088bc2da0f2a6f1f32c0f8
AngelVasilev <angavasilev@gmail.com>
2022-06-30 22:10:46 +0300

    Update Program.cs

--- CSharp - Algorithms Fundamentals/Recursion and Combinations - Exercise/05.School Teams/Program.cs
+++ CSharp - Algorithms Fundamentals/Recursion and Combinations - Exercise/05.School Teams/Program.cs
@@ -1,44 +1,51 @@
-﻿class Program
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SchoolTeams
 {
-    public static void Main()
+    class Program
     {
-        var girls = Console.ReadLine().Split(", ");
-        var girlsComb = new string[3];
-        var girlsCombs = new List<string[]>();
-        GenerateCombinations(0, 0, girls, girlsComb, girlsCombs);
-
-        var boys = Console.ReadLine().Split(", ");
-        var boysComb = new string[2];
-        var boysCombs = new List<string[]>();
-
-        GenerateCombinations(0, 0, boys, boysComb, boysCombs);
-
-        PrintCombinations(girlsCombs, boysCombs);
-    }
-
-    private static void PrintCombinations(List<string[]> girlsCombs, List<string[]> boysCombs)
-    {
-        foreach (var girlComb in girlsCombs)
+        public static void Main()
         {
-            foreach (var boyComb in boysCombs)
+            var girls = Console.ReadLine().Split(", ");
+            var girlsComb = new string[3];
+            var girlsCombs = new List<string[]>();
+            GenerateCombinations(0, 0, girls, girlsComb, girlsCombs);
+
+            var boys = Console.ReadLine().Split(", ");
+            var boysComb = new string[2];
+            var boysCombs = new List<string[]>();
+
+            GenerateCombinations(0, 0, boys, boysComb, boysCombs);
+
+            PrintCombinations(girlsCombs, boysCombs);
+        }
+
+        private static void PrintCombinations(List<string[]> girlsCombs, List<string[]> boysCombs)
+        {
+            foreach (var girlComb in girlsCombs)
             {
-                Console.WriteLine($"{string.Join(", ", girlComb)}, {string.Join(", ", boyComb)}");
+                foreach (var boyComb in boysCombs)
+                {
+                    Console.WriteLine($"{string.Join(", ", girlComb)}, {string.Join(", ", boyComb)}");
+                }
             }
         }
-    }
 
-    private static void GenerateCombinations(int index, int start, string[] elements, string[] combination, List<string[]> combinations)
-    {
-        if (index >= combination.Length)
+        private static void GenerateCombinations(int index, int start, string[] elements, string[] combination, List<string[]> combinations)
         {
-            combinations.Add(combination.ToArray());
-            return;
-        }
+            if (index >= combination.Length)
+            {
+                combinations.Add(combination.ToArray());
+                return;
+            }
 
-        for (int i = start; i < elements.Length; i++)
-        {
-            combination[index] = elements[i];
-            GenerateCombinations(index + 1, i + 1, elements, combination, combinations);
+            for (int i = start; i < elements.Length; i++)
+            {
+                combination[index] = elements[i];
+                GenerateCombinations(index + 1, i + 1, elements, combination, combinations);
+            }
         }
     }
 }
