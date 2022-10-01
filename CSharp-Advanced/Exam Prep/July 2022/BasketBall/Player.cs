947b289ab60d1fe13d1aaba62e1e76f6addd3e31
AngelVasilev <angavasilev@gmail.com>
2022-10-01 08:25:56 +0300

    Create Player.cs

---
+++ CSharp-Advanced/Exam Prep/July 2022/BasketBall/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Basketball
+{
+    using System.Text;
+
+    public class Player
+    {
+        public Player(string name, string position, double rating, int games)
+        {
+            this.Name = name;
+            this.Position = position;
+            this.Rating = rating;
+            this.Games = games;
+        }
+        
+        public string Name { get; set; }
+        
+        public string Position { get; set; }
+        
+        public double Rating { get; set; }
+        
+        public int Games { get; set; }
+
+        public bool Retired { get; set; } = false;
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"-Player: {this.Name}")
+                .AppendLine($"--Position: {this.Position}")
+                .AppendLine($"--Rating: {this.Rating}")
+                .AppendLine($"--Games played: {this.Games}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
